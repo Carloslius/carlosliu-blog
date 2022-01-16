3a63229a1b0fa19b64f82e00be23329318aecbc6
“Carlosliu” <1753459461@qq.com>
2022-01-16 16:25:34 +0800

    初始化项目

---
+++ blog-api/src/main/java/plus/carlosliu/config/MPConfig.java
@@ -0,0 +1,20 @@
+package plus.carlosliu.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("plus.carlosliu.mapper")
+public class MPConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+}
